984e3b040b9751ef1c281e2ecb297851280d73f9
claudchan <claud.chan@gmail.com>
2017-08-29 11:31:31 +0800

    Update jQuery.stickyFooter.js

--- jQuery.stickyFooter.js
+++ jQuery.stickyFooter.js
@@ -3,54 +3,74 @@
  * Copyright (c) 2017 claudchan
  * MIT license
  */
+;(function ($, window, document, undefined) {
 
-(function ($) {
+  var pluginName = 'stickyFooter',
+      defaults = {
+        wrapper: $('html'),
+        container: $('body'),
+        delay: 200
+      };
 
-  $.fn.stickyFooter = function (options) {
+  function Plugin(element, options) {
+    this.element = element;
+    this.options = $.extend( {}, defaults, options );
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.init();
+  }
 
-    var settings = $.extend({}, $.fn.stickyFooter.defaults, options),
-        element = this;
+  $.extend(Plugin.prototype, {
 
-    stickyFooter(element, settings);
-
-    return this;
-   
-  };
-
-  function stickyFooter(element, settings) {
-
-    $this = $(element);
-    wrapper = settings.wrapper;
-    container = settings.container;
-    delay = settings.delay;
-
-    var update = function () {
-      wrapper.css({ 'position': 'relative', 'min-height': '100%' });
-      $this.css({ 'position': 'absolute', 'bottom': '0', 'margin': '0', 'width': '100%' });
-      container.css('margin-bottom', $this.outerHeight());
+    init: function () {
+      this.stickyFooter();
     },
+    stickyFooter: function () {
+      var element = $(this.element),
+      wrapper = this.options.wrapper,
+      container = this.options.container,
+      delay = this.options.delay,
+      _update = function () {
+        wrapper.css({ 'position': 'relative', 'min-height': '100%' });
+        element.css({ 'position': 'absolute', 'bottom': '0', 'margin': '0', 'width': '100%' });
+        container.css('margin-bottom', element.outerHeight());
+      },
+      resize = false;
+      _update();
+      $(window).on('resize', function () {
+        resize = true;
+      });
+      setInterval(function () {
+        if(resize) {
+          resize = false;
+          _update();
+        }
+      }, delay);
+    }
+  });
 
-    resize = false;
-
-    update();
-
-    $(window).on('resize', function () {
-      resize = true;
-    });
-
-    setInterval(function () {
-      if(resize) {
-        resize = false;
-        update();
-      }
-    }, delay);
-
+  $.fn[pluginName] = function ( options ) {
+    var args = arguments;
+    if (options === undefined || typeof options === 'object') {
+      return this.each(function () {
+        if (!$.data(this, 'plugin_' + pluginName)) {
+          $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+        }
+      });
+    }
+    else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
+      var returns;
+      this.each(function () {
+        var instance = $.data(this, 'plugin_' + pluginName);
+        if (instance instanceof Plugin && typeof instance[options] === 'function') {
+            returns = instance[options].apply( instance, Array.prototype.slice.call( args, 1 ) );
+        }
+        if (options === 'destroy') {
+          $.data(this, 'plugin_' + pluginName, null);
+        }
+      });
+      return returns !== undefined ? returns : this;
+    }
   };
 
-  $.fn.stickyFooter.defaults = {
-    wrapper: $('html'),
-    container: $('body'),
-    delay: 200
-  };
-
-})(jQuery);
+})(jQuery, window, document);
